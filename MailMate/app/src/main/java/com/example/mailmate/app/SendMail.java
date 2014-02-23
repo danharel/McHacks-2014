26adb4f5da4a156431da56c50bdd4cb917a05126
Brian Yang <brian@brianyang.me>
2014-02-22 22:14:55 -0500

    Add Android project with SendGrid

---
+++ MailMate/app/src/main/java/com/example/mailmate/app/SendMail.java
@@ -0,0 +1,35 @@
+package com.example.mailmate.app;
+
+import com.github.sendgrid.SendGrid;
+
+/**
+ * Created by Brian on 2/22/14.
+ */
+public class SendMail {
+
+    public static void send(final String to, final String subject, final String msg) {
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+
+                    SendGrid sendgrid = new SendGrid("brianyang", "McHacks_2014");
+
+                    sendgrid.addTo(to);
+                    sendgrid.setFrom("test@example.com");
+                    sendgrid.setSubject(subject);
+                    sendgrid.setText(msg);
+
+                    sendgrid.send();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+
+    }
+}
